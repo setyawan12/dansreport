e703d0714b4cf75763c67561924c924c6bc1385f
setyawan12 <bocahgendut005@gmail.com>
2024-07-11 20:26:32 +0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Report = require('../models/report');
+const path = require('path');
+const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
-    const reports = await Report.find({});
-    res.render('index', { reports });
+    try {
+        const reports = await Report.find({});
+        res.render('index', { reports });  // Pastikan 'index' sesuai dengan nama file view
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = router;

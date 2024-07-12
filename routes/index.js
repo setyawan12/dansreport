eb494b8c57d3cd7e61b267e33f18a8c07c169fed
setyawan12 <bocahgendut005@gmail.com>
2024-07-12 09:16:34 +0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Report = require('../models/report');
-const moment = require('moment-timezone'); // Tambahkan ini
 
 router.get('/', async (req, res) => {
     const reports = await Report.find({});
-    const formattedReports = reports.map(report => ({
-        ...report.toObject(),
-        reportTime: moment(report.reportTime).tz('Asia/Jakarta').format('DD/MM/YYYY, h:mm:ss A') // Format waktu WIB
-    }));
-    res.render('index', { reports: formattedReports });
+    res.render('index', { reports });
 });
 
 module.exports = router;
